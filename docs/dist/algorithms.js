1485b743516d32edd8f634ffbbe8cc3f42caf2aa
Snowpack Build <actions@github.com>
2022-09-14 20:13:30 +0000

    Commit from GitHub Actions (Snowpack)

--- docs/dist/algorithms.js
+++ docs/dist/algorithms.js
@@ -1,4 +1,5 @@
 import * as cons from "./constants.js";
+import {Queue} from "./queue.js";
 function inBounds(x, y) {
   if (x >= 0 && x < cons.GRID_WIDTH && y >= 0 && y < cons.GRID_HEIGHT) {
     return true;
@@ -6,14 +7,11 @@ function inBounds(x, y) {
   return false;
 }
 export function* depthFirstSearch(grid, start) {
-  console.log("DFS START:", start);
-  console.log("grid:", grid);
   const stack = [start];
   const DIRS = [[-1, 0], [0, 1], [1, 0], [0, -1]];
   while (stack.length !== 0) {
     const [cell_x, cell_y] = stack.pop();
     if (grid[cell_y][cell_x] === 3) {
-      console.log("BREAKBREAKBREAK");
       break;
     }
     if (grid[cell_y][cell_x] === 2) {
@@ -25,6 +23,30 @@ export function* depthFirstSearch(grid, start) {
       const [new_x, new_y] = [cell_x + dir_x, cell_y + dir_y];
       if (inBounds(new_x, new_y) && (grid[new_y][new_x] === 0 || grid[new_y][new_x] === 3)) {
         stack.push([new_x, new_y]);
+      }
+    });
+  }
+  yield grid;
+}
+export function* breadthFirstSearch(grid, start) {
+  const q = new Queue([start]);
+  console.log("q", q);
+  const DIRS = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+  while (!q.isEmpty) {
+    const [cell_x, cell_y] = q.dequeue();
+    if (grid[cell_y][cell_x] === 3) {
+      break;
+    }
+    if (grid[cell_y][cell_x] === 2) {
+    } else {
+      grid[cell_y][cell_x] = 1;
+    }
+    yield grid;
+    DIRS.forEach(([dir_x, dir_y]) => {
+      const [new_x, new_y] = [cell_x + dir_x, cell_y + dir_y];
+      if (inBounds(new_x, new_y) && (grid[new_y][new_x] === 0 || grid[new_y][new_x] === 3)) {
+        console.log(new_x, new_y);
+        q.enqueue([new_x, new_y]);
       }
     });
   }
