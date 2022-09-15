6d1f8aa79a2ba14fd23664a671c3eb00cabb6c37
Snowpack Build <actions@github.com>
2022-09-15 17:26:27 +0000

    Commit from GitHub Actions (Snowpack)

--- docs/dist/algorithms.js
+++ docs/dist/algorithms.js
@@ -8,18 +8,15 @@ function inBounds(x, y) {
 }
 export function* depthFirstSearch(grid, start) {
   const stack = [start];
-  const DIRS = [[-1, 0], [0, 1], [1, 0], [0, -1]];
   while (stack.length !== 0) {
     const [cell_x, cell_y] = stack.pop();
     if (grid[cell_y][cell_x] === 3) {
       break;
-    }
-    if (grid[cell_y][cell_x] === 2) {
-    } else {
+    } else if (grid[cell_y][cell_x] !== 2 && grid[cell_y][cell_x] !== 4) {
       grid[cell_y][cell_x] = 1;
     }
     yield grid;
-    DIRS.forEach(([dir_x, dir_y]) => {
+    cons.DFS_DIRS.forEach(([dir_x, dir_y]) => {
       const [new_x, new_y] = [cell_x + dir_x, cell_y + dir_y];
       if (inBounds(new_x, new_y) && (grid[new_y][new_x] === 0 || grid[new_y][new_x] === 3)) {
         stack.push([new_x, new_y]);
@@ -30,22 +27,20 @@ export function* depthFirstSearch(grid, start) {
 }
 export function* breadthFirstSearch(grid, start) {
   const q = new Queue([start]);
-  console.log("q", q);
-  const DIRS = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+  const visited = new Set();
   while (!q.isEmpty) {
     const [cell_x, cell_y] = q.dequeue();
     if (grid[cell_y][cell_x] === 3) {
       break;
-    }
-    if (grid[cell_y][cell_x] === 2) {
-    } else {
+    } else if (grid[cell_y][cell_x] !== 2 && grid[cell_y][cell_x] !== 4) {
       grid[cell_y][cell_x] = 1;
     }
     yield grid;
-    DIRS.forEach(([dir_x, dir_y]) => {
+    cons.BFS_DIRS.forEach(([dir_x, dir_y]) => {
       const [new_x, new_y] = [cell_x + dir_x, cell_y + dir_y];
-      if (inBounds(new_x, new_y) && (grid[new_y][new_x] === 0 || grid[new_y][new_x] === 3)) {
-        console.log(new_x, new_y);
+      const new_cell_id = new_y * cons.GRID_WIDTH + new_x;
+      if (inBounds(new_x, new_y) && (grid[new_y][new_x] === 0 || grid[new_y][new_x] === 3) && !visited.has(new_cell_id)) {
+        visited.add(new_cell_id);
         q.enqueue([new_x, new_y]);
       }
     });
