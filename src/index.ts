6cdc6d63e8d13096f0bd49d0f7c4f0fa40755889
Cole <cebCole@gmail.com>
2022-10-05 13:54:29 -0500

    fixed bug where instant button breaks, when no algo selected

--- src/index.ts
+++ src/index.ts
@@ -269,8 +269,10 @@ pauseButton?.addEventListener('click', pauseLoop, false)
 
 
 function instantFunc() {
-  myGlobal.i = myGlobal.animation.length-1
-  updateCanvas(myGlobal.animation[myGlobal.i], myGlobal.ctx)
+  if (myGlobal.animation.length != 0) {
+    myGlobal.i = myGlobal.animation.length-1
+    updateCanvas(myGlobal.animation[myGlobal.i], myGlobal.ctx)
+  }
 }
 
 const instantButton: HTMLElement = document.getElementById('instant')!
